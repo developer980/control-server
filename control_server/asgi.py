69a7fbb30f5a6318e6608f9c189b9993d058650e
developer980 <tudorcernat22@gmail.com>
2025-11-30 00:43:05 +0200

    added asgi configuration to allow the app to accept http and websocket connections

--- control_server/asgi.py
+++ control_server/asgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+from channels.security.websocket import AllowedHostsOriginValidator
+from control_server.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control_server.settings')
+django.setup()
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket":
+       AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+    ),
+})

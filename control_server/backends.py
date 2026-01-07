4258c94a66708b236572fb148d74f246536ac89f
developer980 <tudorcernat22@gmail.com>
2026-01-08 00:43:02 +0200

    minor improvements for LoginBackend

--- control_server/backends.py
+++ control_server/backends.py
@@ -2,10 +2,10 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 class LoginBackend(ModelBackend):
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, request, username=None, password=None, is_staff=False):
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(email=username)
+            user = UserModel.objects.get(email=username, is_staff=is_staff)
         except UserModel.DoesNotExist:
             return None
         else:

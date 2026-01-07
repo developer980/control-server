d2a15f07151f3439ea70a6b2301f00c490ec4b5c
developer980 <tudorcernat22@gmail.com>
2026-01-08 00:37:54 +0200

    added sidebar element for admin dashboard

---
+++ static/scripts/sidebar.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $("#add_user").click(function () {
+    window.location.href = "/c-admin/add-user";
+  });
+  $("#users").click(function () {
+    window.location.href = "/c-admin/users";
+  });
+  $("#vehicles").click(function () {
+    window.location.href = "/c-admin/vehicles";
+  });
+  $("#add_vehicle").click(function () {
+    window.location.href = "/c-admin/add_vehicle";
+  });
+});

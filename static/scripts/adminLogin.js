a63ac2f85af8dd3a9ac0e69a0d1d5cd1d4aa2800
developer980 <tudorcernat22@gmail.com>
2026-01-07 13:17:21 +0200

    added adminLogin.js script

---
+++ static/scripts/adminLogin.js
@@ -0,0 +1,36 @@
+const csrf_token = document.cookie
+  .split("; ")
+  .find((row) => row.startsWith("csrftoken="))
+  .split("=")[1];
+
+$(document).ready(function () {
+  console.log("Admin login script loaded");
+  $("#login-form").on("submit", async function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const email = $("#email").val();
+    const password = $("#password").val();
+    const route = `http://${window.location.host}/api/c-admin/auth/login/`;
+    const data = await fetch(route, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrf_token,
+      },
+    });
+
+    const response = await data.json();
+
+    if (response.status === "ok") {
+      // Redirect to otp page
+      console.log("response:", response);
+      window.location.href = response.redirect_url;
+    } else {
+      alert(`Error: ${response.message}`);
+    }
+  });
+});

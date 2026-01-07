327d6f4a4c1cb294ec5bf295421a4715b734b1ba
developer980 <tudorcernat22@gmail.com>
2026-01-07 13:15:53 +0200

    added is_staff parameter for separating users from admins in authenticate_user() function

--- control_server/controllers/user_controller.py
+++ control_server/controllers/user_controller.py
@@ -4,15 +4,13 @@ from django.contrib.auth import authenticate
 # from requests import request
 import json
 
-def authenticate_user(request):
+def authenticate_user(request, is_staff= False):
     body = request.body
     email = json.loads(body).get("email")
     password = json.loads(body).get("password")
-    user = authenticate(request, username=email, password=password)
+    user = authenticate(request, username=email, password=password, is_staff=is_staff)
 
     if user is not None:
-        # TODO: Add email verification if authentication succesful
-        # login(request, user=user)
         print("Authenticated user:", user.id)
         return {"status": "ok",
                 "message": "We sent a code to your email.", 
@@ -24,9 +22,16 @@ def authenticate_user(request):
 
 def register_user(user_data):
     username = user_data['first_name'].lower() + '_' + user_data['last_name'].lower()
+
+    existing_username = User.objects.filter(username=username).first()
+    sufix = 0
+    while existing_username is not None:
+        sufix += 1
+        existing_username = User.objects.filter(username=username + str(sufix)).first()
+    
     
     user = User.objects.create_user(
-        username=username,
+        username=username + str(sufix),
         email=user_data['email'],
         first_name=user_data.get('first_name', ''),
         last_name=user_data.get('last_name', ''),
@@ -51,3 +56,10 @@ def fetch_users(email="", first_name="", last_name="", username=""):
         users = users.filter(username=username)
     
     return users.order_by('id')
+
+def fetch_user_by_id(user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        return user
+    except User.DoesNotExist:
+        return None

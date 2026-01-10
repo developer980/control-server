c6d320ae5d13781bcd2484addfb52e594b17979d
developer980 <tudorcernat22@gmail.com>
2026-01-10 15:56:02 +0200

    added password management methods for vehicle_model

--- control_server/models/vehicle_model.py
+++ control_server/models/vehicle_model.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 class Vehicle(models.Model):
     """
@@ -6,9 +7,15 @@ class Vehicle(models.Model):
     """
     
     created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
-    vehicle_id = models.IntegerField(unique=True)
     vehicle_type = models.CharField(max_length=50, default='utility')
 
+    def generate_password(self, password):
+        self.password = make_password(password)
+
+    def compare_password(self, password):
+        return check_password(password, self.password)
+
     def __str__(self):
-        return self.name
+        return self.vehicle_type
